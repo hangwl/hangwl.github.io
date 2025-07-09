3f5b43ffb429b25e1f8e916f02b32fd71a0f7034
hangwl <hangweilow@gmail.com>
2025-07-09 17:45:13 +0800

    build: :rocket: add github pages build scripts, dependencies, and hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createRouter, createHashHistory } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
@@ -9,6 +9,7 @@ import '@/styles/app.css'
 import reportWebVitals from './reportWebVitals.ts'
 
 // Create a new router instance
+const hashHistory = createHashHistory();
 const router = createRouter({
   routeTree,
   context: {},
@@ -16,6 +17,7 @@ const router = createRouter({
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultPreloadStaleTime: 0,
+  history: hashHistory,
 })
 
 // Register the router instance for type safety
